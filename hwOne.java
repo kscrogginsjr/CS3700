0609d29afc149a0a9ee9303ef8833f2ab63d8d1f
Kevin Scroggins <kscrogginsjr@gmail.com>
2020-02-10 17:56:44 -0800

    Changed to say Goodbye

--- hwOne.java
+++ hwOne.java
@@ -3,6 +3,6 @@ public class hwOne{
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, my name is Kevin");
+		System.out.println("Goodbye, I finished my CS 3700 HW1");
 	}
 }
